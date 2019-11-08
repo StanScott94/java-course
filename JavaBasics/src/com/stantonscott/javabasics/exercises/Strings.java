73911d6b0641b9d6c147f62abdfbacc3e831a142
Busch <josefine.busch@posteo.de>
2019-11-08 11:04:08 +0100

    my solution for strings and some refactoring in userInput

--- JavaBasics/src/com/stantonscott/javabasics/exercises/Strings.java
+++ JavaBasics/src/com/stantonscott/javabasics/exercises/Strings.java
@@ -4,84 +4,115 @@ package com.stantonscott.javabasics.exercises;
  * @author stantonscott
  * @version 1.0
  *
- * <p>Creation Date: 29.10.19</p>
- * <p>Project Name: JavaBasics</p>
+ *          <p>
+ *          Creation Date: 29.10.19
+ *          </p>
+ *          <p>
+ *          Project Name: JavaBasics
+ *          </p>
  */
 public class Strings {
 
-    /**
-     * Get the string that is the longest
-     * @param string1
-     * @param string2
-     * @return longest of two strings
-     */
-    public String  getLongestString(String string1, String string2) {
-        //code goes here
-        return null;
-    }
+	/**
+	 * Get the string that is the longest
+	 * 
+	 * @param string1
+	 * @param string2
+	 * @return longest of two strings
+	 */
+	public String getLongestString(final String string1, final String string2) {
+		return (string1.length() < string2.length()) ? string2 : string1;
+	}
 
-    /**
-     * Check if a string contains another string
-     * @param string1 reference string
-     * @param string2 string to check
-     * @return true if string1 contains string2
-     */
-    public boolean string1ContainsString2(String string1, String string2) {
-        //code goes here
-        return false;
-    }
+	/**
+	 * Check if a string contains another string
+	 * 
+	 * @param string1 reference string
+	 * @param string2 string to check
+	 * @return true if string1 contains string2
+	 */
+	public boolean string1ContainsString2(final String string1, final String string2) {
+		return string1.contains(string2) ? true : false;
+	}
 
-    /**
-     * Get only letters contained in the string (no numbers)
-     * @param string1 string containing letters and numbers
-     * @return string containing only letters
-     */
-    public String getLettersOnly(String string1) {
-        //code goes here
-        return null;
-    }
-    /**
-     * Get the character from the given string at the index supplied
-     * @param string
-     * @param index
-     * @return character at index
-     */
-    public char getCharacterAtIndex(String string, int index) {
-        //code goes here
-        return 0;
-    }
+	/**
+	 * Get only letters contained in the string (no numbers)
+	 * 
+	 * @param string1 string containing letters and numbers
+	 * @return string containing only letters
+	 */
+	public String getLettersOnly(final String string1) {
+		return string1.chars().
+				filter((asciiCode) -> isLetter(asciiCode)).
+				mapToObj((code)->Character.toString((char)code))
+				.reduce("", (a,b)->a+b);
+	}
 
-    /**
-     * Check if a supplied string ends with the letters of another string
-     * @param string1 reference string
-     * @param string2 string to check
-     * @return true if reference string ends with string to check
-     */
-    public boolean doesFirstStringEndWithSecondString(String string1, String string2) {
-        //code goes here
-        return false;
-    }
+	private boolean isLetter(final int asciiCode) {
+		return isUppercaseLetter(asciiCode) || isLowercaseLetter(asciiCode) ? true : false;
+	}
 
-    /**
-     * Get first string in alphabetical order
-     * @param string1
-     * @param string2
-     * @return first alphabetically ordered string
-     */
-    public String getFirstAlphabeticalString(String string1, String string2) {
-        //code goes here
-        return null;
-    }
+	private boolean isLowercaseLetter(final int asciiCode) {
+		return asciiCode >= 97 && asciiCode <= 122;
+	}
 
-    /**
-     * if the supplied String has instances of the reference replace the instance with the replacement
-     * @param reference string to check for
-     * @param string string to check
-     * @param replacementString string to replace reference with
-     * @return string with reference replaced by replacementString
-     */
-    public String replaceInstancesOfReferenceString(String reference, String string, String replacementString) {
-        //code goes here
-        return null;
-    }
+	private boolean isUppercaseLetter(final int asciiCode) {
+		return asciiCode >= 65 && asciiCode <= 90;
+	}
+
+	/**
+	 * Get the character from the given string at the index supplied
+	 * 
+	 * @param string
+	 * @param index
+	 * @return character at index
+	 */
+	public char getCharacterAtIndex(final String string, final int index) {
+		return string.charAt(index);
+	}
+
+	/**
+	 * Check if a supplied string ends with the letters of another string
+	 * 
+	 * @param string1 reference string
+	 * @param string2 string to check
+	 * @return true if reference string ends with string to check
+	 */
+	public boolean doesFirstStringEndWithSecondString(final String string1, final String string2) {
+		return string1.endsWith(string2);
+	}
+
+	/**
+	 * Get first string in alphabetical order
+	 * 
+	 * @param string1
+	 * @param string2
+	 * @return first alphabetically ordered string
+	 */
+	public String getFirstAlphabeticalString(final String string1, final String string2) {
+		return helper(string1, string2, 0);
+	}
+
+	private String helper(final String string1, final String string2, final int index) {
+		if (string1.toLowerCase().charAt(index) < string2.toLowerCase().charAt(index)) {
+			return string1;
+		} else if (string1.toLowerCase().charAt(index) == string2.toLowerCase().charAt(index)) {
+			return helper(string1, string2, index + 1);
+		} else {
+			return string2;
+		}
+	}
+
+	/**
+	 * if the supplied String has instances of the reference replace the instance
+	 * with the replacement
+	 * 
+	 * @param reference         string to check for
+	 * @param string            string to check
+	 * @param replacementString string to replace reference with
+	 * @return string with reference replaced by replacementString
+	 */
+	public String replaceInstancesOfReferenceString(String reference, String string, String replacementString) {
+		return string.replaceAll(reference, replacementString);
+	}
 }
--- JavaBasics/src/com/stantonscott/javabasics/playground/userInput.java
+++ JavaBasics/src/com/stantonscott/javabasics/playground/userInput.java
@@ -8,32 +8,47 @@ import java.util.Scanner;
  * @author stantonscott
  * @version 1.0
  *
- * <p>Creation Date: 29.10.19</p>
- * <p>Project Name: JavaBasics</p>
+ *          <p>
+ *          Creation Date: 29.10.19
+ *          </p>
+ *          <p>
+ *          Project Name: JavaBasics
+ *          </p>
  */
 public class userInput {
 
-    private static final String HELP = "help";
-    private static final String TIME = "time";
+	private static final String HELP = "help";
+	private static final String TIME = "time";
+	private static final String QUIT = "quit";
 
-    public static void main(String[] args) {
-        Scanner userInputScanner = new Scanner(System.in); // Create a Scanner object to get keyboard input
-        while(true) {
-            System.out.println("Enter text and press Enter");
+	public static void main(String[] args) {
+		// Create a Scanner object to get keyboard input
+		Scanner userInputScanner = new Scanner(System.in);
+		try {
+			boolean isRunning = true;
+			while (isRunning) {
+				System.out.println("Enter text and press Enter");
 
-            String userInput = userInputScanner.nextLine(); // Read user input
-            System.out.println("Text entered by user: " + userInput); // Output user input
+				String userInput = userInputScanner.nextLine(); // Read user input
+				System.out.println("Text entered by user: " + userInput); // Output user input
 
-            if (userInput.toLowerCase().equals(HELP)) {
-                System.out.println("Help menu");
-                System.out.println("- help = Help menue");
-                System.out.println("- time = Print current time");
-                System.out.println("create a help menu and maybe your own commands");
-            } else if (userInput.toLowerCase().equals(TIME)) {
-                SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
-                Date date = new Date(System.currentTimeMillis());
-                System.out.println(formatter.format(date));
-            }
-        }
-    }
+				if (userInput.toLowerCase().equals(HELP)) {
+					System.out.println("Help menu");
+					System.out.println("- help = Help menue");
+					System.out.println("- time = Print current time");
+					System.out.println("- quit = Quit the program");
+					System.out.println("create a help menu and maybe your own commands");
+				} else if (userInput.toLowerCase().equals(TIME)) {
+					SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+					Date date = new Date(System.currentTimeMillis());
+					System.out.println(formatter.format(date));
+				} else if (userInput.toLowerCase().equals(QUIT)) {
+					System.out.println("Good Bye Traveler");
+					isRunning = false;
+				}
+			}
+		} finally {
+			userInputScanner.close();
+		}
+	}
 }
