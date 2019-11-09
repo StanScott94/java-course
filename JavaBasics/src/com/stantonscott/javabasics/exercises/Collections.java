339ed400fa0af1b96947be51b38b0cb9e4fbef5a
Stanton <twinstanton@gmail.com>
2019-11-09 23:10:16 +0100

    refactor

--- JavaBasics/src/com/stantonscott/javabasics/exercises/Collections.java
+++ JavaBasics/src/com/stantonscott/javabasics/exercises/Collections.java
@@ -11,84 +11,84 @@ import java.util.Map;
  * <p>Project Name: JavaBasics</p>
  */
 public class Collections {
-
-    /**
-     * Get a string array
-     * @param string1
-     * @param string2
-     * @param string3
-     * @param string4
-     * @return array of strings
-     */
+	
+	/**
+	 * Get a string array
+	 * @param string1
+	 * @param string2
+	 * @param string3
+	 * @param string4
+	 * @return array of strings
+	 */
 	public String[] createStringArray(String string1, String string2, String string3, String string4) {
 		//code goes here
 		return null;
 	}
 	
-    /**
-     * Get an int array
-     * @param int1
-     * @param int2
-     * @param int3
-     * @param int4
-     * @return array of ints
-     */
+	/**
+	 * Get an int array
+	 * @param int1
+	 * @param int2
+	 * @param int3
+	 * @param int4
+	 * @return array of ints
+	 */
 	public int[] createIntArray(int int1, int int2, int int3, int int4) {
 		//code goes here
 		return null;
 	}
 	
-    /**
-     * Get a list of strings
-     * @param string1
-     * @param string2
-     * @param string3
-     * @param string4
-     * @return list of strings
-     */
+	/**
+	 * Get a list of strings
+	 * @param string1
+	 * @param string2
+	 * @param string3
+	 * @param string4
+	 * @return list of strings
+	 */
 	public List<String> createArrayListOfStrings(String string1, String string2, String string3, String string4) {
 		//code goes here
 		return null;
 	}
 	
-    /**
-     * Get a map with string keys and int values
-     * @param key1
-     * @param key2
-     * @param key3
-     * @param value1
-     * @param value2
-     * @param value3
-     * @return array of ints
-     */
+	/**
+	 * Get a map with string keys and int values
+	 * @param key1
+	 * @param key2
+	 * @param key3
+	 * @param value1
+	 * @param value2
+	 * @param value3
+	 * @return array of ints
+	 */
 	public Map<String, Integer> createMap(String key1, String key2, String key3, int value1, int value2, int value3) {
 		//code goes here
 		return null;
 	}
 	
-    /**
-     * Replace the value of a key value map
-     * @param map
-     * @param keyToFind
-     * @param newValue
-     * @return map with value replaced for key
-     */
-    public Map<String, Integer> findAndReplace(Map<String, Integer> map, String keyToFind, int newValue) {
-    	//code goes here
-        return null;
-    }
-    
-    /**
-     * remove the value from a map
-     * @param map
-     * @param keyToFind
-     * @return map with value removed
-     */
-    public Map<String, Integer> removeEntry(Map<String, Integer> map, String keyToFind) {
-    	//code goes here
-        return null;
-    }
-    
+	/**
+	 * Replace the value of a key value map
+	 * @param map
+	 * @param keyToFind
+	 * @param newValue
+	 * @return map with value replaced for key
+	 */
+	public Map<String, Integer> findAndReplace(Map<String, Integer> map, String keyToFind, int newValue) {
+		//code goes here
+	    return null;
+	}
+	
+	/**
+	 * remove the value from a map
+	 * @param map
+	 * @param keyToFind
+	 * @return map with value removed
+	 */
+	public Map<String, Integer> removeEntry(Map<String, Integer> map, String keyToFind) {
+		//code goes here
+	    return null;
+	}
+	
 	/**
 	 * Remove entries that have the given char in the key
 	 * @param map
