170c4582f9c3c87f5fa30ee40861066594006570
Stanton <twinstanton@gmail.com>
2019-11-09 21:59:20 +0100

    setting up collection exercises

---
+++ JavaBasics/tstsrc/com/stantonscott/javabasics/exercises/CollectionsTest.java
@@ -0,0 +1,66 @@
+package com.stantonscott.javabasics.exercises;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+/**
+ * @author stantonscott
+ * @version 1.0
+ *
+ * <p>Creation Date: 29.10.19</p>
+ * <p>Project Name: JavaBasics</p>
+ */
+public class CollectionsTest {
+	
+	Collections collections = new Collections();
+
+	@Test
+	public String[] createStringArray() {
+		//code goes here
+		return null;
+	}
+	
+	@Test
+	public int[] createIntArray() {
+		//code goes here
+		return null;
+	}
+	
+	@Test
+	public List<String> createArrayListOfStrings() {
+		//code goes here
+		return null;
+	}
+	
+	@Test
+	public Map<String, Integer> createMap() {
+		//code goes here
+		return null;
+	}
+	
+	@Test
+    public Map<String, Integer> findAndReplace() {
+    	//code goes here
+        return null;
+    }
+    
+	@Test
+    public Map<String, Integer> removeValue() {
+    	//code goes here
+        return null;
+    }
+    
+	@Test
+	public Map<String, String> removeEntry() {
+		//code goes here
+		return null;
+	}
+	
+	@Test
+	public Map<String, String> replaceValue() {
+		//code goes here
+		return null;
+	}
+}
